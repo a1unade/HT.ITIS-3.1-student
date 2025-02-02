511bbf3c039d78ab9da5d00c19f0386f4f8a703e
? <tags0956@gmail.com>
2025-02-02 20:34:05 +0300

    [add] cqrs, repositories, unit_of_work

--- Dotnet.Homeworks.MainProject/Configuration/RabbitMqConfig.cs
+++ Dotnet.Homeworks.MainProject/Configuration/RabbitMqConfig.cs
@@ -2,7 +2,7 @@ namespace Dotnet.Homeworks.MainProject.Configuration;
 
 public class RabbitMqConfig
 {
-    public string Username { get; set; }
-    public string Password { get; set; }
-    public string Hostname { get; set; }
+    public string Username { get; set; } = "";
+    public string Password { get; set; } = "";
+    public string Hostname { get; set; } = "";
 }
